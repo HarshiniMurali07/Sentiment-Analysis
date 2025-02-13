b1730c46c83f440b449af6a899d0844835cee705
Harshini Murali <160369949+HarshiniMurali07@users.noreply.github.com>
2025-02-13 13:41:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ st.title('Movie Review Sentiment Analysis')
 review = st.text_area('Enter a movie review:')
 
 def main():
-        if review.strip():
-            transformed_review = vectorizer.transform([review])
-            prediction = model.predict(transformed_review)[0]
-            sentiment = 'Positive' if prediction == 'positive' else 'Negative'
-            st.write(f'The sentiment is: **{sentiment}**')
-        else:
-            st.write('Please enter a valid review.')
+    if review.strip():
+        transformed_review = vectorizer.transform([review])
+        prediction = model.predict(transformed_review)[0]
+        return 'Positive' if prediction == 'positive' else 'Negative'
+    else:
+        return None
 
 if __name__ == "__main__":
     if st.button('Predict'):
-        main()
-
-# myenv\Scripts\activate
-# streamlit run app.py        
+        sentiment = main()
+        if sentiment:
+            st.write(f'The sentiment is: **{sentiment}**')
+        else:
+            st.write('Please enter a valid review.')
