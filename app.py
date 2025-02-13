7149326b0a6b7cfc4d91fd00df8e9855aa4638af
Harshini Murali <160369949+HarshiniMurali07@users.noreply.github.com>
2025-02-13 13:34:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import joblib
 
@@ -9,11 +8,18 @@ vectorizer = joblib.load('tfidf_vectorizer.pkl')
 st.title('Movie Review Sentiment Analysis')
 review = st.text_area('Enter a movie review:')
 
-if st.button('Predict'):
-    if review.strip():
-        transformed_review = vectorizer.transform([review])
-        prediction = model.predict(transformed_review)[0]
-        sentiment = 'Positive' if prediction == 'positive' else 'Negative'
-        st.write(f'The sentiment is: **{sentiment}**')
-    else:
-        st.write('Please enter a valid review.')
+def main():
+        if review.strip():
+            transformed_review = vectorizer.transform([review])
+            prediction = model.predict(transformed_review)[0]
+            sentiment = 'Positive' if prediction == 'positive' else 'Negative'
+            st.write(f'The sentiment is: **{sentiment}**')
+        else:
+            st.write('Please enter a valid review.')
+
+if __name__ == "__main__":
+    if st.button('Predict'):
+        main()
+
+# myenv\Scripts\activate
+# streamlit run app.py        
